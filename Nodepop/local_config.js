7046f60fbf27b710f58a03524ddd6de7ef780436
Alicia Daza <amdazacastillo@gmail.com>
2016-05-07 00:04:47 +0200

    Trying restrict tags

--- Nodepop/local_config.js
+++ Nodepop/local_config.js
@@ -1,8 +1,16 @@
 /**
  * Created by Alicia on 03/05/2016.
  */
+
+// Secret key for JWT Auth
 module.exports = {
     jwt: {
         secret: 'fkY5Ud7AJSLS'
     }
 };
+/*
+// Default values for advertisement tags
+module.exports = {
+    tagValues: ["work", "lifestyle", "motor", "mobile"]
+};
+*/
