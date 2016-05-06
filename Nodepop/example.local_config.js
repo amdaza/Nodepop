1da707efcac05ae3797e3cada18fa8c18f6a0f98
Alicia Daza <amdazacastillo@gmail.com>
2016-05-07 00:08:49 +0200

    added default tag values

--- Nodepop/example.local_config.js
+++ Nodepop/example.local_config.js
@@ -1,8 +1,11 @@
 /**
  * Created by Alicia on 03/05/2016.
  */
+
+// Secret key for JWT Auth
 module.exports = {
     jwt: {
         secret: 'fkY5Ud7AJSLS'
-    }
+    },
+    tagValues: ["work", "lifestyle", "motor", "mobile"]
 };
